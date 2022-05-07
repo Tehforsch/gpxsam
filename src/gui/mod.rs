08a991be930b1c29dab3c0702f9fd9304c0351bf
tehforsch <mail@tonipeter.de>
2022-05-07 12:18:50 +0200

    allow showing the original tracks with a checkbox

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -4,18 +4,21 @@ use gpx::Gpx;
 
 mod chunk_plotter;
 
+use crate::lib::chunk::Chunk;
 use crate::lib::segment_splitter::SegmentSplitter;
 
 use self::chunk_plotter::ChunkPlotter;
 
 pub struct Gui {
     files: Vec<Gpx>,
+    show_files: bool,
 }
 
 impl Gui {
     pub fn new(files: &[Gpx]) -> Gui {
         Gui {
             files: files.to_vec(),
+            show_files: false,
         }
     }
 }
@@ -28,9 +31,20 @@ impl epi::App for Gui {
     fn update(&mut self, ctx: &egui::CtxRef, _: &epi::Frame) {
         let splitter = SegmentSplitter::from_gpx(self.files.iter());
         egui::CentralPanel::default().show(ctx, |ui| {
+            ui.checkbox(&mut self.show_files, "Show files");
             let mut plot = ChunkPlotter::new();
-            for chunk in splitter.chunks {
-                plot.add_chunk(chunk)
+            if self.show_files {
+                for file in self.files.iter() {
+                    for track in file.tracks.iter() {
+                        for segment in track.segments.iter() {
+                            plot.add_chunk(Chunk::from_entire_segment(segment));
+                        }
+                    }
+                }
+            } else {
+                for chunk in splitter.chunks {
+                    plot.add_chunk(chunk)
+                }
             }
             plot.show(ui);
         });
